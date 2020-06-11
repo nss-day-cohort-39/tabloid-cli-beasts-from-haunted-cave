951ebbea0167b8abf2664e2c89a4564dac32f094
blake slochowsky <blakeslochowsky.com>
2020-06-11 15:02:29 -0500

    main menu done

--- TabloidCLI/UserInterfaceManagers/MainMenuManager.cs
+++ TabloidCLI/UserInterfaceManagers/MainMenuManager.cs
@@ -9,9 +9,11 @@ namespace TabloidCLI.UserInterfaceManagers
 
         public IUserInterfaceManager Execute()
         {
+            Console.WriteLine("Hello, Please select a option from below.");
+
             Console.WriteLine("Main Menu");
 
-            Console.WriteLine(" 1) Journal Management");
+            Console.WriteLine(" 1) My Journal Management");
             Console.WriteLine(" 2) Blog Management");
             Console.WriteLine(" 3) Author Management");
             Console.WriteLine(" 4) Post Management");

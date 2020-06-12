db4ea3dc54aeca897679c1e19e08e36e3354079c
Mark McCann <mjmccann12@me.com>
2020-06-12 14:22:47 -0500

    add remove and list are working

--- TabloidCLI/Repositories/BlogRepository.cs
+++ TabloidCLI/Repositories/BlogRepository.cs
@@ -1,10 +1,171 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using Microsoft.Data.SqlClient;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
 
-namespace TabloidCLI.UserInterfaceManagers
+namespace TabloidCLI
 {
-    class BlogManager
+    public class BlogRepository : DatabaseConnector, IRepository<Blog>
     {
+        public BlogRepository(string connectionString) : base(connectionString) { }
+
+        public List<Blog> GetAll()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id,
+                                               Title,
+                                               Url
+                                          FROM Blog";
+
+                    List<Blog> blogs = new List<Blog>();
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Blog newBlog = new Blog()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            Url = reader.GetString(reader.GetOrdinal("Url"))
+                        };
+                        blogs.Add(newBlog);
+                    }
+
+                    reader.Close();
+
+                    return blogs;
+                }
+            }
+        }
+
+        public Blog Get(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT b.Id AS BlogId,
+                                               b.Title,
+                                               b.Url,
+                                               
+                                          FROM Blog b 
+                                         WHERE a.id = @id";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    Blog blog = null;
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        if (blog == null)
+                        {
+                            blog = new Blog()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Title = reader.GetString(reader.GetOrdinal("Title")),
+                                Url = reader.GetString(reader.GetOrdinal("Url")),
+                            };
+                        }
+                    }
+
+                    reader.Close();
+
+                    return blog;
+                }
+            }
+        }
+
+        public void Insert(Blog blog)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Blog (Title, Url)
+                                                     VALUES (@title, @url)";
+                    cmd.Parameters.AddWithValue("@title", blog.Title);
+                    cmd.Parameters.AddWithValue("@url", blog.Url);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Update(Blog blog)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"UPDATE Blog 
+                                           SET Title = @title,
+                                               Url = @url,
+                                         WHERE id = @id";
+
+                    cmd.Parameters.AddWithValue("@title", blog.Title);
+                    cmd.Parameters.AddWithValue("@url", blog.Url);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"DELETE FROM Blog WHERE id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+
+        //public void InsertBlog(Blog blog, Tag tag)
+        //{
+        //    using (SqlConnection conn = Connection)
+        //    {
+        //        conn.Open();
+        //        using (SqlCommand cmd = conn.CreateCommand())
+        //        {
+        //            cmd.CommandText = @"INSERT INTO BlogTag (BlogId, TagId)
+        //                                               VALUES (@blogId, @tagId)";
+        //            cmd.Parameters.AddWithValue("@blogId", blog.Id);
+        //            cmd.Parameters.AddWithValue("@tagId", tag.Id);
+        //            cmd.ExecuteNonQuery();
+        //        }
+        //    }
+        //}
+
+        //public void DeleteBlog(int blogId, int tagId)
+        //{
+        //    using (SqlConnection conn = Connection)
+        //    {
+        //        conn.Open();
+        //        using (SqlCommand cmd = conn.CreateCommand())
+        //        {
+        //            cmd.CommandText = @"DELETE FROM BlogTag
+        //                                 WHERE BlogId = @blogid AND 
+        //                                       TagId = @tagId";
+        //            cmd.Parameters.AddWithValue("@blogId", blogId);
+        //            cmd.Parameters.AddWithValue("@tagId", tagId);
+
+        //            cmd.ExecuteNonQuery();
+        //        }
+        //    }
+        //}
     }
 }
